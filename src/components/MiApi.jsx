337ccf2b953c1b7076bdc415236fec23f5e1cd90
Makaosva <maka.osses@gmail.com>
2024-06-15 01:18:34 -0400

    feat/Cambio en css y arreglos

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Table, Button } from "react-bootstrap";
 import Buscador from "./Buscador";
 
 function MiApi() {
-  const [data, setData] = useState([]);
+  const [info, setInfo] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [query, setQuery] = useState("");
 
-  const url = "https://www.swapi.tech/api/people";
+  const url1 = "https://www.swapi.tech/api/people";
+  const url2 = "https://www.swapi.tech/api/planets";
+  const url3 = "https://www.swapi.tech/api/starships";
 
   useEffect(() => {
     consultaDeAPI();
@@ -15,11 +19,10 @@ function MiApi() {
 
   const consultaDeAPI = async () => {
     try {
-      const response = await fetch(url);
-      const data = await response.json();
-      setData(data.results);
+      const response = await fetch(url1);
+      const info = await response.json();
+      setInfo(info.results);
       setLoading(false);
-      console.log("DATA---->", data);
     } catch (error) {
       console.error(error.message);
       setError(error);
@@ -27,10 +30,16 @@ function MiApi() {
     }
   };
 
-  const filteredData = data.filter((people) =>
+  console.log("INFO---->", info);
+  console.info("tipo de dato=", typeof info);
+
+  const filteredData = info.filter((people) =>
     people.name.toLowerCase().includes(query.toLowerCase())
   );
-  const sortedData = filteredData.sort((a, b) => a.name.localeCompare(b.name));
+  // const sortedData = filteredData.sort((a, b) => a.uid.localeCompare(b.uid));
+  const sortedData = filteredData.sort(function (a, b) {
+    return a.uid - b.uid;
+  });
 
   if (loading) {
     return <div>Loading</div>;
@@ -40,17 +49,43 @@ function MiApi() {
   }
 
   return (
-    <div>
-      <h2>Lista de Personajes de Star</h2>
-      <Buscador query={query} setQuery={setQuery} />
-      <ul>
-        {sortedData.map((personaje) => (
-          <li key={personaje.name}>
-            <a href={personaje.url}>{personaje.name}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <h1>Lista de Personajes Star Wars</h1>
+      <Container fluid className="container">
+        <Row>
+          <Col className="buscador">
+            <Buscador query={query} setQuery={setQuery} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={5} sm={12} className="listado">
+            <Table responsive striped bordered className="table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nombre</th>
+                  <th>Ver Detalle</th>
+                </tr>
+              </thead>
+              <tbody>
+                {sortedData.map((personaje) => (
+                  <tr key={personaje.uid}>
+                    <td>{personaje.uid}</td>
+                    <td>{personaje.name}</td>
+                    <td>
+                      <Button>Detalle</Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Col>
+          <Col md={7} sm={12}>
+            <p>Detalle Prsonaje</p>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
